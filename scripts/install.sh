440ae4ac9bf5b461d4de5fd215c4521bca4040b6
fioncat <lazycat7706@gmail.com>
2022-11-07 13:00:26 +0800

    install: add prompt

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,6 +14,16 @@ if [ ! -d $packer_home ]; then
 	git clone https://github.com/wbthomason/packer.nvim $packer_home
 fi
 
+read -p "About to open nvim to install plugins, please exit after installation done, press any key to continue... " -n 1 -r
 nvim +'PackerInstall'
-python3 ./scripts/snapshot/rollback.py
+
+read -p "Do you want to use plugins snapshot? This can make nvim more stable (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	python3 ./scripts/snapshot/rollback.py
+fi
+
+read -p "About ot open nvim to finish installation, press any key to continue... " -n 1 -r
 nvim +'PackerCompile'
+
+echo "Done, happy using spacenvim!"
