f1629dd360cc9f4c72499a85689c3880492e6420
fioncat <lazycat7706@gmail.com>
2022-11-13 20:48:59 +0800

    update release job

---
+++ scripts/github-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ue
+
+RELEASE_VERSION=${GITHUB_REF#refs/*/}
+
+IFS='-' read -r -a VERS <<< "${RELEASE_VERSION}"
+SPACENVIM_VERSION=${VERS[0]}
+NEOVIM_VERSION=${VERS[1]}
+
+if [ -z ${NEOVIM_VERSION} ]; then
+	echo "invalid release version ${RELEASE_VERSION}"
+	exit 1
+fi
+
+RELEASE_TITLE="${SPACENVIM_VERSION} for neovim ${NEOVIM_VERSION}"
+echo "RELEASE_TITLE=\"${RELEASE_TITLE}\"" >> ${GITHUB_ENV}
+
