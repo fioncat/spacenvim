37971547db89314f966bd9ea0d6a6724dd2f4235
wenqian <lazycat7706@gmail.com>
2022-11-14 19:23:51 +0800

    build: rebuild build scripts (#4)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
+# Donot allow undefined variables
 set -u
 
 DEST_PATH="${HOME}/.config/nvim"
 DEST_URL="https://github.com/fioncat/spacenvim.git"
-BACKUP_DIR=$(pwd)
 
-# string formatters
+PACKER_ROOT="${HOME}/.local/share/nvim/site/pack/packer"
+PACKER_PATH="${PACKER_ROOT}/start/packer.nvim"
+PACKER_URL="https://github.com/wbthomason/packer.nvim"
+
 if [[ -t 1 ]]; then
 	tty_escape() { printf "\033[%sm" "$1"; }
 else
 	tty_escape() { :; }
 fi
 
-tty_mkbold() { tty_escape "1;$1"; }
-tty_underline="$(tty_escape "4;39")"
+# TTY color placeholders. Usage: ${tty_*}xxx${tty_reset}
 tty_yellow="$(tty_escape "0;33")"
-tty_blue="$(tty_mkbold 34)"
-tty_red="$(tty_mkbold 31)"
-tty_bold="$(tty_mkbold 39)"
+tty_blue="$(tty_escape "0;34")"
+tty_red="$(tty_escape "0;31")"
+tty_cyan="$(tty_escape "0;36")"
 tty_reset="$(tty_escape 0)"
 
-abort() {
-	cd ${BACKUP_DIR}
-	printf "%s\n" "$@" >&2
+on_error() {
+	printf "${tty_red}error:${tty_reset} $@\n" >&2
 	exit 1
 }
 
@@ -38,97 +39,79 @@ shell_join() {
 }
 
 execute() {
+	shell_exec=$(shell_join "$@")
+	printf "==> ${tty_cyan}${shell_exec}${tty_reset}\n"
 	if ! "$@"; then
-		abort "$(printf "${tty_red}Failed during${tty_reset}: %s" "$(shell_join "$@")")"
+		on_error "command ${tty_cyan}${shell_exec}${tty_reset} exited with error"
 	fi
 }
 
-getc() {
-	local save_state
-	save_state="$(/bin/stty -g)"
-	/bin/stty raw -echo
-	IFS='' read -r -n 1 -d '' "$@"
-	/bin/stty "${save_state}"
+ensure_command() {
+	if ! command -v $1 >/dev/null; then
+		on_error "command ${tty_cyan}$1${tty_reset} is required to install spacenvim, please install it first."
+	fi
 }
 
-wait_for_user() {
-	local c
+confirm() {
+	read -p "$1 (y/n) " -n 1 -r
 	echo
-	echo "Press ${tty_bold}RETURN${tty_reset}/${tty_bold}ENTER${tty_reset} to continue or any other key to abort..."
-	getc c
-	# we test for \r and \n because some stuff does \r instead
-	if ! [[ "${c}" == $'\r' || "${c}" == $'\n' ]]; then
-		echo "${tty_red}User aborted${tty_reset}"
-		exit 1
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		return 0
 	fi
+	if [ $2 == "true" ]; then
+		on_error "user aborted"
+	fi
+	return 1
 }
 
-if ! command -v perl >/dev/null; then
-	abort "$(
-		cat <<EOABORT
-Perl is required to interpret this script. See:
-  ${tty_underline}https://www.perl.org/get.html${tty_reset}
-EOABORT
-	)"
-fi
+hint() {
+	echo "* ${tty_yellow}$1${tty_reset}"
+}
 
-if ! command -v nvim >/dev/null; then
-	abort "$(
-		cat <<EOABORT
-You must install neovim before installing Spacenvim. See:
-  ${tty_underline}https://github.com/neovim/neovim/wiki/Installing-Neovim${tty_reset}
-EOABORT
-	)"
-fi
+ensure_command "perl"
+ensure_command "git"
+ensure_command "nvim"
+ensure_command "python3"
 
-if ! command -v git >/dev/null; then
-	abort "$(
-		cat <<EOABORT
-You must install Git before installing this Nvim config. See:
-  ${tty_underline}https://git-scm.com/${tty_reset}
-EOABORT
-	)"
+confirm "Install Spacenvim?" "true"
+if [ -d ${DEST_PATH} ]; then
+	confirm "Found an existing neovim config ${tty_blue}${DEST_PATH}${tty_reset}, remove it?" "false"
+	if [ $? == 0 ]; then
+		execute "rm" "-r" "${DEST_PATH}"
+	fi
 fi
-
 if [ ! -d ${DEST_PATH} ]; then
-	printf "About to clone spacenvim to ${tty_blue}%s${tty_reset}" "${DEST_PATH}"
-	wait_for_user
-	execute "git" "clone" ${DEST_URL} ${DEST_PATH}
-	echo
+	execute "git" "clone" "${DEST_URL}" "${DEST_PATH}"
 fi
-cd ${DEST_PATH} || return
 
-PACKER_ROOT="${HOME}/.local/share/nvim/site/pack/packer"
-PACKER_PATH="${PACKER_ROOT}/start/packer.nvim"
 if [ ! -d ${PACKER_PATH} ]; then
-	printf "About to clone wbthomason/packer.nvim to ${tty_blue}%s${tty_reset}" "${PACKER_PATH}"
-	wait_for_user
-	execute "git" "clone" "https://github.com/wbthomason/packer.nvim" "${PACKER_PATH}"
-	echo
-fi
-
-cat <<EOS
-Now we will open nvim to install plugins, This process may take a long time.
-If any plugin installation fails, you MUST re-execute ${tty_blue}:PackerInstall${tty_reset} manually.
-EOS
-printf "${tty_yellow}Please quit nvim after all plugins are installed successfully${tty_reset}"
-wait_for_user
-nvim +PackerInstall
-
-echo
-read -p "Do you want to use plugins snapshot? This can make nvim more stable (require python3) (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	command -v python3 >/dev/null || { echo "ERROR: cannot find python3"; exit 1; }
-	python3 ./hack/snapshot.py rollback
+	execute "git" "clone" "${PACKER_URL}" "${PACKER_PATH}"
 fi
 
 echo
-echo "Now we will open nvim to compile plugins"
-printf "${tty_yellow}Please quit nvim after plugins compilation done${tty_reset}"
-wait_for_user
-nvim +PackerCompile
+hint "Now we will open nvim to install plugins, this may take a long time."
+hint "If any plugin installation fails, you MUST execute PackerInstall"
+hint "manually to make sure all plugins are installed successfully."
+hint "Otherwise nvim will report error."
+confirm "Please quit nvim after installation done, continue?" "true"
+execute "nvim" "+PackerInstall"
+
+echo
+hint "Use recommended versions will pin all plugins versions to snapshot."
+hint "This can make nvim more stable."
+hint "If you want to update plugins, please execute PackerSync in nvim."
+hint "If you want to rollback plugins, please run:"
+hint "  cd ~/.config/nvim; make rollback"
+confirm "Do you want to use recommended versions for plugins?" "false"
+if [ $? == 0 ]; then
+	execute "cd" "${DEST_PATH}"
+	execute "make" "rollback"
+fi
+
+echo
+hint "Now we will open nvim to compile plugins."
+confirm "Please quit nvim after compilation done, continue" "true"
+execute "nvim" "+PackerCompile"
 
 echo
 echo "Spacenvim was successfully installed to your machine! Thanks for using."
-cd ${BACKUP_DIR}
