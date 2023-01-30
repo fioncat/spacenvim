8d3510eca990d27ad024c6c1f361c805849f5900
fioncat <lazycat7706@gmail.com>
2023-01-30 11:30:51 +0800

    feat: update plugins version and config

--- lua/plugins/common/gitsigns.lua
+++ lua/plugins/common/gitsigns.lua
@@ -33,7 +33,7 @@ return function()
 			},
 		},
 		current_line_blame = true,
-		current_line_blame_opts = { delay = 1000, virtual_text_pos = "eol" },
+		current_line_blame_opts = { delay = 1000, virt_text_pos = "eol" },
 		current_line_blame_formatter = "    <author>, <author_time:%Y-%m-%d> - <summary>",
 		current_line_blame_formatter_nc = "    <author>",
 		sign_priority = 6,
--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -20,32 +20,14 @@ return function()
 		end
 	end
 
-	local function get_palette()
-		if vim.g.colors_name == "catppuccin" then
-			-- If the colorscheme is catppuccin then use the palette.
-			return require("catppuccin.palettes").get_palette()
-		else
-			-- Default behavior: return lspsaga's default palette.
-			local palette = require("lspsaga.lspkind").colors
-			palette.peach = palette.orange
-			palette.flamingo = palette.orange
-			palette.rosewater = palette.yellow
-			palette.mauve = palette.violet
-			palette.sapphire = palette.blue
-			palette.maroon = palette.orange
-
-			return palette
-		end
-	end
-
 	set_sidebar_icons()
 
-	local colors = get_palette()
+	local colors = require("utils").get_palette()
 
 	require("lspsaga").setup({
 		preview = {
 			lines_above = 1,
-			lines_below = 12,
+			lines_below = 17,
 		},
 		scroll_preview = {
 			scroll_down = "<C-j>",
@@ -79,12 +61,12 @@ return function()
 			sign = true,
 			enable_in_insert = true,
 			sign_priority = 20,
-			virtual_text = true,
+			virtual_text = false,
 		},
 		diagnostic = {
-			twice_into = false,
 			show_code_action = false,
 			show_source = true,
+			jump_num_shortcut = true,
 			keys = {
 				exec_action = "<CR>",
 				quit = "q",
@@ -93,11 +75,10 @@ return function()
 		},
 		rename = {
 			quit = "<C-c>",
-			exec = "<CR>",
 			mark = "x",
 			confirm = "<CR>",
-			whole_project = true,
-			in_select = true,
+			exec = "<CR>",
+			in_select = false,
 		},
 		outline = {
 			win_position = "right",
@@ -114,12 +95,11 @@ return function()
 			},
 		},
 		symbol_in_winbar = {
-			in_custom = true,
 			enable = false,
-			separator = " " .. icons.ui.Separator,
-			hide_keyword = true,
-			show_file = false,
-			color_mode = true,
+		},
+		beacon = {
+			enable = true,
+			frequency = 12,
 		},
 		ui = {
 			theme = "round",
@@ -132,28 +112,13 @@ return function()
 			diagnostic = icons.ui.Bug,
 			incoming = icons.ui.Incoming,
 			outgoing = icons.ui.Outgoing,
-			colors = {
-				normal_bg = colors.base,
-				title_bg = colors.base,
-				red = colors.red,
-				megenta = colors.maroon,
-				orange = colors.peach,
-				yellow = colors.yellow,
-				green = colors.green,
-				cyan = colors.sapphire,
-				blue = colors.blue,
-				purple = colors.mauve,
-				white = colors.text,
-				black = colors.mantle,
-				fg = colors.text,
-			},
 			kind = {
 				-- Kind
 				Class = { icons.kind.Class, colors.yellow },
 				Constant = { icons.kind.Constant, colors.peach },
 				Constructor = { icons.kind.Constructor, colors.sapphire },
 				Enum = { icons.kind.Enum, colors.yellow },
-				EnumMember = { icons.kind.EnumMember, colors.rosewater },
+				EnumMember = { icons.kind.EnumMember, colors.teal },
 				Event = { icons.kind.Event, colors.yellow },
 				Field = { icons.kind.Field, colors.teal },
 				File = { icons.kind.File, colors.rosewater },
