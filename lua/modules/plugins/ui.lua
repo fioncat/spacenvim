797f6385944d4685df7dadcd17ac0cb9228415c7
fioncat <lazycat7706@gmail.com>
2023-11-01 10:46:07 +0800

    update plugins

--- lua/modules/plugins/ui.lua
+++ lua/modules/plugins/ui.lua
@@ -37,6 +37,7 @@ ui["lukas-reineke/indent-blankline.nvim"] = {
 	lazy = true,
 	event = "BufReadPost",
 	config = require("ui.indent-blankline"),
+	pin = true,
 }
 
 ui["nvim-lualine/lualine.nvim"] = {
