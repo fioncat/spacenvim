3443e97e4ab69f0e1d124ba73aec49bc6431f831
fioncat <lazycat7706@gmail.com>
2023-12-18 16:41:35 +0800

    feat: Update keymaps for buffer

--- lua/keymap/ui.lua
+++ lua/keymap/ui.lua
@@ -7,8 +7,9 @@ local map_cr = bind.map_cr
 local plug_map = {
 	-- Plugin: bufferline
 	["n|<leader>bb"] = map_cr("BufferLinePick"):with_noremap():with_silent(),
-	["n|<leader>bn"] = map_cr("BufferLineCycleNext"):with_noremap():with_silent(),
-	["n|<leader>bp"] = map_cr("BufferLineCyclePrev"):with_noremap():with_silent(),
+	["n|<leader>bd"] = map_cr("BufferLinePickClose"):with_noremap():with_silent(),
+	-- ["n|<leader>bn"] = map_cr("BufferLineCycleNext"):with_noremap():with_silent(),
+	-- ["n|<leader>bp"] = map_cr("BufferLineCyclePrev"):with_noremap():with_silent(),
 }
 
 bind.nvim_load_mapping(plug_map)
