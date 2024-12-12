639833ac82aa71bf682b3ad75554cfcf100e05a6
fioncat <lazycat7706@gmail.com>
2024-12-12 15:32:13 +0800

    fix: https://github.com/neovim/neovim/issues/30985

--- lua/modules/configs/completion/mason-lspconfig.lua
+++ lua/modules/configs/completion/mason-lspconfig.lua
@@ -59,6 +59,17 @@ M.setup = function()
 		end
 	end
 
+	-- Fix: https://github.com/neovim/neovim/issues/30985
+	for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+		local default_diagnostic_handler = vim.lsp.handlers[method]
+		vim.lsp.handlers[method] = function(err, result, context, config)
+			if err ~= nil and err.code == -32802 then
+				return
+			end
+			return default_diagnostic_handler(err, result, context, config)
+		end
+	end
+
 	mason_lspconfig.setup_handlers({ mason_lsp_handler })
 end
 
